237ba13496ccc69e92dcfd33f72d343967068ad3
Billel ZOUAK <billelzouak35@gmail.com>
2026-07-25 16:14:48 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 
 const SUPABASE_CONFIG = {
   url: 'https://iguuxkodbvegnocqvfso.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlndXV4a29kYnZlZ25vY3F2ZnNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ5ODQ2MTUsImV4cCI6MjEwMDU2MDYxNX0.s954UrmstKudcgWCkfUq3-oWAYjL9oYVdpsc1FOyUUs',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlndXV4a29kYnZlZ25vY3F2ZnNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ5ODQ2MTUsImV4cCI6MjEwMDU2MDYxNX0.s954UrmstKudcgWCkfUq3-oWAYjL9oYVdpsc1FOyUUs'
 };
 
 // Export for use in other files
